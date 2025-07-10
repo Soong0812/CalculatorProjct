ee411d3bb0c6d1542ba745966a0cf3d54cfdd2dd
Soong0812 <shotpo90@gmail.com>
2025-07-10 13:36:16 +0900

    계산기Lv.1틀

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -7,17 +7,52 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("계산기를 시작합니다.");
+        while (true) {
+            System.out.println("계산기를 시작합니다.");
 
-        System.out.println("첫번째 수를 입력하세요 : ");
-        int num1 = scanner.nextInt();
+            System.out.println("첫번째 수를 입력하세요 : ");
+            int num1 = scanner.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요 : ");
-        char operations = scanner.next().charAt(0);
+            System.out.println("사칙연산 기호를 입력하세요 : ");
+            char operations = scanner.next().charAt(0);
 
-        System.out.println("두번째 수를 입력하세요 : ");
-        int num2 = scanner.nextInt();
+            System.out.println("두번째 수를 입력하세요 : ");
+            int num2 = scanner.nextInt();
 
+            int result = 0;
+            boolean valid = true;
+
+            switch (operations) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("적용하지 않은 연산 기호입니다.");
+                    valid = false;
+                    break;
+        }
+
+        if (valid) {
+            System.out.println("결과 : " + result);
+        }
+
+            System.out.println("더 계산하시겠습니까? (Y/N)");
+            String next = scanner.next();
+            if (next.equalsIgnoreCase("N")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+        }
+    scanner.close();
     }
 
 }
