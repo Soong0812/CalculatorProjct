4adbe72999e1be3c6ed375a778e804a9acb7c6ed
Soong0812 <shotpo90@gmail.com>
2025-07-10 14:57:21 +0900

    계산기Lv.1

--- src/main/java/org/example/CalculatorLv1.java
+++ src/main/java/org/example/CalculatorLv1.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Calculator {
+public class CalculatorLv1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
