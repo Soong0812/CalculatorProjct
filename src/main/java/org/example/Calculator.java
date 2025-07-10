b4b1f537e3875211d15438fbdbacfe7fe900d50b
Soong0812 <shotpo90@gmail.com>
2025-07-10 14:55:18 +0900

    계산기Lv.1

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -33,17 +33,22 @@ public class Calculator {
                     result = num1 * num2;
                     break;
                 case '/':
-                    result = num1 / num2;
+                    if (num2 == 0) {
+                        System.out.println("나눗셈에서 분모에 0은 입력할 수 없습니다.");
+                        valid = false;
+                    } else {
+                        result = num1 / num2;
+                    }
                     break;
                 default:
                     System.out.println("적용하지 않은 연산 기호입니다.");
                     valid = false;
                     break;
-        }
+                }
 
         if (valid) {
             System.out.println("결과 : " + result);
-        }
+            }
 
             System.out.println("더 계산하시겠습니까? 종료는 'exit' 를 입력해주세요.");
             String next = scanner.next();
